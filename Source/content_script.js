35ce808c6d4f31bfe791ef143c3bbda0f2bc2dfa
Ralph Shao <ralph@ralphshao.com>
2014-06-02 10:28:33 -0400

    be a little more efficient

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
+	var nowareRunning = false;
 	var initNoware = function() {
-		$('a').mouseover(function() {
-			var href_url = this.href;
-			href_url = decodeURIComponent(href_url.replace(/https?:\/\/t\.yesware\.com\/tl(\/[0-9a-f]+)+\?ytl=(.+)/gi, "$2"));
-			$(this).attr('href', href_url);
+		if (nowareRunning) return;
+		nowareRunning = true;
+		$('a:not(.noware-replaced)').each(function() {
+			$(this).addClass('noware-replaced');
+			if (this.href.indexOf('yesware') >= 0) {
+				var href_url = this.href;
+				href_url = decodeURIComponent(href_url.replace(/https?:\/\/t\.yesware\.com\/tl(\/[0-9a-f]+)+\?ytl=(.+)/gi, "$2"));
+				if (this.href != href_url) {
+					$(this).attr('href', href_url);
+				}
+			}
 		});
+		nowareRunning = false;
 	};
 	$(document).bind('DOMSubtreeModified', function() {
 		initNoware();
